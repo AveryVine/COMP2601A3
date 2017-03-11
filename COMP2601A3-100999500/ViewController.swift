3a9c11467cb2f555a468560a317077ddfbadb66a
Avery Vine <averyvine@cmail.carleton.ca>
2017-03-11 14:24:16 -0500

    Added initial UI elements

--- COMP2601A3-100999500/ViewController.swift
+++ COMP2601A3-100999500/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView0: UIImageView?
+    @IBOutlet var imageView1: UIImageView?
+    @IBOutlet var imageView2: UIImageView?
+    @IBOutlet var imageView3: UIImageView?
+    @IBOutlet var imageView4: UIImageView?
+    @IBOutlet var imageView5: UIImageView?
+    @IBOutlet var imageView6: UIImageView?
+    @IBOutlet var imageView7: UIImageView?
+    @IBOutlet var imageView8: UIImageView?
+    @IBOutlet var label: UILabel?
+    @IBOutlet var button: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
